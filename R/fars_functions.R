31c8b7fab2fe2b7a5a4f4b2803e0f5702cede63b
Katie <katie.key@colostate.edu>
2017-11-06 21:48:13 -0700

    pdf generated

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -4,7 +4,6 @@
 # test_trend_ca
 # test_trend_log_reg
 
-library(dplyr)
 library(stats) 
 library(dplyr) 
 library(broom)
@@ -12,7 +11,7 @@ library(broom)
 #####################################################
 
 perc_cis <- function(x = x, n = n){
-  library(scales)
+  library(scales) 
   
   proportion <-  x/n
   est_of_stand <-  sqrt((proportion*(1 - proportion))/n)
@@ -28,8 +27,6 @@ perc_cis_vec <- paste(prop, " ", "(", low_conf, ",", " ",upp_conf, ")", sep = ""
 return(perc_cis_vec)
 }
 
-perc_cis(x = 9000, n = 23000)
-# [1] "39.1% (38.5%, 39.8%)"
 
 #####################################################
 
@@ -56,53 +53,50 @@ test_trend_ca <- function(drug, data = clean_fars) {
   Z <-  sqrt(ca_drug_type$statistic)
   p.value <- ca_drug_type$p.value
   
+  Z <- round(Z, digits = 1)
+  p.value <- round(p.value, digits = 3)
+  
   test_trend_table <- tibble::tibble(Z, p.value)
   
   test_trend <- test_trend_table %>% 
-    mutate(Z= signif(Z, digits = 2)) %>%
     mutate(Z = as.numeric(Z)) %>% 
-    mutate(p.value = round(p.value, digits = 3)) %>%
     mutate(p.value = as.numeric(p.value))
   
   return(test_trend)
 }
 
-test_trend_ca(drug = "Alcohol")
-test_trend_ca(drug = "Nonalcohol")
-test_trend_ca(drug = "Stimulant")
 
 #####################################################
 
 test_trend_log_reg <- function(drug, data = clean_fars) {
-    if(drug == "Nonalcohol"){
-      to_test <- clean_fars %>%
-        filter(!is.na(drug_type)) %>% 
-        filter(drug_type != "Alcohol") %>% 
-        group_by(unique_id, year) %>% 
-        summarize(positive_for_drug = any(positive_for_drug))
-      
-} else{
-      to_test <- clean_fars %>%
-        filter(!is.na(drug_type)) %>%
-        filter(drug_type == drug)
-}
-    log_reg <- glm(positive_for_drug ~ year, data = to_test,
-                   family = binomial(link = "logit"))
-    log_reg_clean <- slice(tidy(log_reg), 2)
-    Z <- log_reg_clean$statistic
-    p.value <- log_reg_clean$p.value
+  if(drug == "Nonalcohol"){
+    to_test <- clean_fars %>%
+      filter(!is.na(drug_type)) %>% 
+      filter(drug_type != "Alcohol") %>% 
+      group_by(unique_id, year) %>% 
+      summarize(positive_for_drug = any(positive_for_drug))
     
-    log_reg_table <- tibble::tibble(Z, p.value)
-    
-    log_reg_trend <- log_reg_table %>% 
-      mutate(Z= signif(Z, digits = 2)) %>%
-      mutate(Z = as.numeric(Z)) %>% 
-      mutate(p.value = round(p.value, digits = 3)) %>%
-      mutate(p.value = as.numeric(p.value))
-    
-   return(log_reg_trend)
+  } else{
+    to_test <- clean_fars %>%
+      filter(!is.na(drug_type)) %>%
+      filter(drug_type == drug)
+  }
+  log_reg <- glm(positive_for_drug ~ year, data = to_test,
+                 family = binomial(link = "logit"))
+  log_reg_clean <- slice(tidy(log_reg), 2)
+  Z <- log_reg_clean$statistic
+  p.value <- log_reg_clean$p.value
+  
+  Z <- round(Z, digits = 1)
+  p.value <- round(p.value, digits = 3)
+  
+  log_reg_table <- tibble::tibble(Z, p.value)
+  
+  log_reg_trend <- log_reg_table %>% 
+    mutate(Z = as.numeric(Z)) %>% 
+    mutate(p.value = as.numeric(p.value))
+  
+  return(log_reg_trend)
 }
 
-test_trend_log_reg(drug = "Alcohol")
-test_trend_log_reg(drug = "Nonalcohol")
 
